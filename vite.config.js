ad3161072d4f85bb3ca28ce221edad28924d78ef
HoHuynhTrung <hohuynhtrung2003@gmail.com>
2026-09-05 16:20:20 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
-import { defineConfig } from 'vite'
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
-})
+  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  base: "/f8-zoom-node2-fe",
+});
